8ea05fd8261b3b9c1c3fd206e68c28ffa1600488
markzhl <417279881@qq.com>
2017-09-11 17:02:51 +0800

    Add files via upload

---
+++ shared-api/src/main/java/com/github/markzhl/api/vo/user/UserInfo.java
@@ -0,0 +1,72 @@
+package com.github.markzhl.api.vo.user;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 
+ *
+ * @author mark
+ * @create 2017-06-21 8:12
+ */
+public class UserInfo implements Serializable{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -7217659554202532388L;
+	public String id;
+    public String username;
+    public String password;
+    public String name;
+    private String description;
+
+    public Date getUpdTime() {
+        return updTime;
+    }
+
+    public void setUpdTime(Date updTime) {
+        this.updTime = updTime;
+    }
+
+    private Date updTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
